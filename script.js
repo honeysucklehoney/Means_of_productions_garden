a48c6972f224885ecffc07a3ba4480a90066d84f
honeysucklehoney <61999030+honeysucklehoney@users.noreply.github.com>
2021-01-16 11:58:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -10,30 +10,34 @@ var fibre_plants = new Array ("Day lily", "Iris", "milkweed", "phorium tanax", "
 var dye_plants = new Array ("tea bush", "elderberry", "marigold", "lupine", "pokeberry", "madder root", "coreopsis", "Mallow hollyhock", "st. john's wort", "japanese indigo/dyers knotweed", "woad", "blood root", "weld");
 var fibre_trees= new Array ("black willow", "bamboo", "yellow willow", "elderberry");
 var food_S = new Array ("tanzi","tea bush", "sunchocke", "creeping buttercup", "cumfrey", "elecampane", "yellow dock", "mustard green", "rosehip", "goutweed", "mock orange",);
-var fruit_trees= new Array ("cherry", "plumtree", "appletree"); //liberty apple and snow apple have been combined due to lack of imagery//
+var fruit_trees= new Array ("Cherry_tree", "Plum tree", "Apple_tree"); //liberty apple and snow apple have been combined due to lack of imagery//
 var bottom_plants = new Array ("tansy", "fireweed", "lupine", "furny stuff");
 var nettle_n_such = new Array ("nettle","organgrapes");
 var nettle_alt = new Array ("nettle2", "nettle3");
+var food = new Array ("Blackberry (Himalayan and thornless)", "Rasberry", "Apple tree", "Plum tree", "Strawberry", "Blueberry", "Cherry tree", "Spruce tree", "Oregon grape", "Elderberry", "Suncoke", "Creeping buttercup","Plantain","Yellow dock", "Mustard Green", "nootka rose (rosehips)","Mock orange", "quince", "Red currant");
+var wood = new Array ("Cherry_tree", "Apple_tree", "Empress tree", "Green willow", "White ash", "Chestnut", "Bamboo (2 clumping varieties, species unknown)", "Ocean Spray (Iron Wood)")
 
 function choosePic() {
 
-var randomNum = Math.floor(Math.random() * food_N.length);
+var randomNum = Math.floor(Math.random() * food.length);
 var randomNum_fruit_trees = Math.floor(Math.random() * fruit_trees.length);
 var randomNum_nettle = Math.floor(Math.random() * nettle_n_such.length);
 var randomNum_food_S = Math.floor(Math.random() * food_S.length);
 var randomNum_dye = Math.floor(Math.random() * dye_plants.length);
 var randomNum_fibre = Math.floor(Math.random() * fibre_plants.length);
+var randomNum_food = Math.floor(Math.random() * food.length);
+var randomNum_wood = Math.floor(Math.random() * wood.length);
  //document.getElementById("food_N_img").src = "/images/" + food_N[randomNum] +  ".jpg";
-var imageUrl = "images/" + food_N[randomNum] + ".jpg";
+var imageUrl = "images/" + food[randomNum] + ".jpg";
 $('.food_N').css('background-image', 'url("' + imageUrl + '")');
 
-var imageUrl = "images/" + food_N2[randomNum] + ".jpg";
+var imageUrl = "images/" + food[randomNum] + ".jpg";
 $('.food_N2').css('background-image', 'url("' + imageUrl + '")');
 
-var imageUrl = "images/" + food_N3[randomNum] + ".jpg";
+var imageUrl = "images/" + food[randomNum] + ".jpg";
 $('.food_N3').css('background-image', 'url("' + imageUrl + '")');
 
-var imageUrl = "images/" + food_N4[randomNum] + ".jpg";
+var imageUrl = "images/" + food[randomNum] + ".jpg";
 $('.food_N4').css('background-image', 'url("' + imageUrl + '")');
 
 var imageUrl = "images/" + nettle_n_such[randomNum_nettle] + ".jpg";
@@ -45,22 +49,25 @@ $('.nettle_alt').css('background-image', 'url("' + imageUrl + '")');
 var imageUrl = "images/" + fruit_trees[randomNum_fruit_trees] + ".jpg";
 $('.fruit_trees').css('background-image', 'url("' + imageUrl + '")');
 
-var imageUrl = "images/" + fruit_trees[randomNum] + ".jpg";
-$('.fruit_trees').css('background-image', 'url("' + imageUrl + '")');
-
 var imageUrl = "images/" + food_S[randomNum] + ".jpg";
 $('.food_S').css('background-image', 'url("' + imageUrl + '")');
 
 
 var imageUrl = "images/" + fibre_plants[randomNum_fibre] + ".jpg";
 $('.fibre_plant_id').css('background-image', 'url("' + imageUrl + '")');
-
 document.getElementById("fibre_plant_id_name").innerHTML = fibre_plants[randomNum_fibre];
 
 var imageUrldye = "images/" + dye_plants[randomNum_dye] + ".jpg";
 $('.dye_plant_id').css('background-image', 'url("' + imageUrldye + '")');
-
 document.getElementById("dye_plant_id_name").innerHTML = dye_plants[randomNum_dye];
+
+var imageUrlfood = "images/" + food[randomNum_food] + ".jpg";
+$('.food_plant_id').css('background-image', 'url("' + imageUrlfood + '")');
+document.getElementById("food_plant_id_name").innerHTML = food[randomNum_food];
+
+var imageUrlwood = "images/" + wood[randomNum_wood] + ".jpg";
+$('.wood_plant_id').css('background-image', 'url("' + imageUrlwood + '")');
+document.getElementById("wood_plant_id_name").innerHTML = wood[randomNum_wood];
 
 }
 
@@ -77,6 +84,11 @@ function myFunction() {
     dots.style.marginTop = "0px";
     $('.Id_formating2').css("left", "100px")
     $('.Id_formating2').css("top", "20%")
+    $('.Id_formating3').css("top", "700px")
+    $('.Id_formating3').css("left", "80%")
+    $('.Id_formating4').css("top", "500px")
+    $('.Id_formating4').css("left", "100px")
+
 
 
 
@@ -87,9 +99,12 @@ function myFunction() {
     dots.style.margin = "0%";
     dots.style.height ="1500px";
     btnText.innerHTML = "Close map";
-  $('.Id_formating2').css("left", "1200px")
-  $('.Id_formating2').css("top", "700px")
-
+  $('.Id_formating2').css("left", "80%")
+  $('.Id_formating2').css("top", "710px")
+  $('.Id_formating3').css("top", "1000px")
+  $('.Id_formating3').css("left", "80%")
+  $('.Id_formating4').css("top", "1320px")
+  $('.Id_formating4').css("left", "80%")
 
     // moreText.style.display = "inline";
   }
@@ -155,31 +170,57 @@ function dye_plants_button() {
   }
 }
 
+function food_plants_button() {
+  var popdownfood = document.getElementById("popdownfood");
+  // var moreText = document.getElementById("letslarger");
 
+  if (popdownfood.style.display === "none") {
+    popdownfood.style.display = "block";
+    $(".food").each(function(){
+           // Test if the div element is empty
+           if($(this).is(":empty")){
+               $(this).css("background", "#476BC7");
+           }
+       });
 
+  }
 
-//document.getElementById("food_N").innerHTML = food_N[randomNum];
+  else {
+    popdownfood.style.display = "none";
+    $(".bushel.food").css("background", "#4d4d00");
+    $(".and_a_peck.food").css("background", "#304701");
+    $(".and_a_barrel.food").css("background", "#698000");
+    $(".andaheap.food").css("background", "#3b5900");
+    $(".quince.food").css("background", "#00292E");
+    $(".heyo.food").css("background", "#4d4d00");
+    $(".windand.food").css("background", "#999900");
+    $(".therain.food").css("background", "#2b2b00");
+    $(".causeiloveu.food").css("background", "#293d01");
+  }
+}
 
-// var randomNum2 = Math.floor(Math.random() * fibre_plants.length);
-//    document.getElementById("cell-a3").src = (fibre_plants[randomNum2] + ".jpg");
-//    document.getElementById("cell-a4").innerHTML = fibre_plants[randomNum2];
-//
-// var randomNum3 = Math.floor(Math.random() * dye_plants.length);
-//     document.getElementById("cell-a3").src = (dye_plants[randomNum3] + ".jpg");
-//     document.getElementById("cell-a4").innerHTML = dye_plants[randomNum3];
-//
-// var randomNum4 = Math.floor(Math.random() * fibre_trees.length);
-//     document.getElementById("cell-a3").src = (fibre_trees[randomNum4] + ".jpg");
-//     document.getElementById("cell-a4").innerHTML = fibre_trees[randomNum4];
-//
-// var randomNum6 = Math.floor(Math.random() * food_S.length);
-//     document.getElementById("cell-a3").src = (food_S[randomNum6] + ".jpg");
-//     document.getElementById("cell-a4").innerHTML = food_S[randomNum6];
-//
-// var randomNum7 = Math.floor(Math.random() * fruit_trees.length);
-//     document.getElementById("cell-a3").src = (fruit_trees[randomNum7] + ".jpg");
-//     document.getElementById("cell-a4").innerHTML = fruit_trees[randomNum7];
-//
-// var randomNum8 = Math.floor(Math.random() * bottom_plants.length);
-//     document.getElementById("cell-a3").src = (bottom_plants[randomNum8] + ".jpg");
-//     document.getElementById("cell-a4").innerHTML = bottom_plants[randomNum8];
+function wood_button() {
+  var popdownwood = document.getElementById("popdownwood");
+  // var moreText = document.getElementById("letslarger");
+
+  if (popdownwood.style.display === "none") {
+    popdownwood.style.display = "block";
+    $(".wood").each(function(){
+           // Test if the div element is empty
+           if($(this).is(":empty")){
+               $(this).css("background", "#3C59F4");
+           }
+       });
+
+  }
+
+  else {
+    popdownwood.style.display = "none";
+    $(".im_talkin.wood").css("background", "#1b2603");
+    $(".inmysleep.wood").css("background", "#baba02");
+    $(".heyo.wood").css("background", "#4d4d00");
+    $(".windand.wood").css("background", "#999900");
+    $(".therain.wood").css("background", "#2b2b00");
+
+  }
+}
